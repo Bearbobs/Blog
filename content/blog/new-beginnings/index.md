0d74e1ab3f9e05b4ee6c27d60673b62997796344
Bearbobs <be10506.16@bitmesra.ac.in>
2020-02-15 22:45:45 +0530

    Setup Basic layout and Initialized with a post

---
+++ content/blog/new-beginnings/index.md
@@ -0,0 +1,19 @@
+---
+title: New Beginnings
+date: "2020-02-15T22:40:32.169Z"
+description: Greetings, hi, BearBobs greets you.
+---
+
+
+## I'm gonna take my horse to the old town road, I'm gonna ride 'til I can't no more ......
+
+
+- printf("Hello World","%s);
+- cout<<"Hello World"<<endl;
+- system.out.println("Hello World")
+- console.log("Hello World")
+- print("Hello World")
+
+Hello, This is Anuj. I am another software engineer.
+I started this blog to document my experiences in Engineering Solutions,
+which will be used in the real world, and solely to improve my writing skills.
---
+++ src/components/layout.js
@@ -0,0 +1,72 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { rhythm, scale } from "../utils/typography"
+
+const Layout = ({ location, title, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  let header
+  const style={"text-align": "center"}
+  if (location.pathname === rootPath) {
+    header = (
+      <h1
+        style={{
+          ...scale(1.5),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: `Montserrat, sans-serif`,
+          marginTop: 0,
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: `none`,
+            textDecoration: `none`,
+            color: `inherit`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    )
+  }
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer style={style}>
+        © {new Date().getFullYear()}, 
+        {` `}
+        <a href="https://github.com/Bearbobs/my-exp-with-SDE">My Escapades With Software Engineering</a>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
