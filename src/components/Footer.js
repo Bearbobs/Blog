cc6b965ea382f964ba239f1c2b8dc6656df03891
bearbobs <be10506.16@bitmesra.ac.in>
2021-03-14 11:32:10 +0530

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,28 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
 import useSiteMetadata from '../hooks/use-site-config'
+import {SOCIAL_LINKS} from './Social.js';
+
+const SocialWrapper = styled.span`
+> a {
+  &:not(:first-child) {
+    margin-left: 1.5rem;
+  }
+`;
+
+const AccessibleText = styled.span`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 1;
+`;
 
 const FooterWrapper = styled.footer`
-  text-align: left;
+  text-align: center;
   padding-top: 30px;
   padding-bottom: 50px;
   background-color: var(--color-primary);
@@ -86,51 +105,40 @@ const FooterWrapper = styled.footer`
 const Footer = () => {
   const { authorName, websiteHost, footerLinks } = useSiteMetadata()
 
-  const FooterItem = ({ item }) => {
-    if (item.url.startsWith('/')) {
-      return (
-        <span className="footer-item">
-          <Link className="footer-link" to={item.url}>
-            {item.label}
-          </Link>
-        </span>
-      )
-    }
-    return (
-      <span className="footer-item">
-        <a className="footer-link" href={item.url} rel={item.rel}>
-          {item.label}
-        </a>
-      </span>
-    )
-  }
-
-  const FooterColumn = ({ column }) => {
-    return (
-      <div className="footer-col">
-        <h3 className="footer-title" key={column.sectionName}>
-          {column.sectionName}
-        </h3>
-        <div className="footer-column-items">
-          {column.links.map((item, i) => {
-            return <FooterItem item={item} key={`footer-column-item-${i}`} />
-          })}
-        </div>
-      </div>
-    )
-  }
-
   return (
     <FooterWrapper>
       <nav>
         <div className="footer-col">
-          <h3 className="footer-title">
+          <p className="footer-item-text">
+            Ready to take the next step and work together?.
+          </p>
+          <p className="footer-item-text">
+            {' '}
+            <a
+              className="footer-link"
+              href="mailto:kapoor.anuj0@gmail.com"
+            >
+              Contact me : <b>kapoor.anuj0@gmail.com</b>
+            </a>
+            .
+          </p>
+          <p className="footer-item-text">
+            <SocialWrapper>
+                {SOCIAL_LINKS.map(({ url, text, Icon }) => (
+                  <a href={url} key={url}>
+                    <Icon size={28} />
+                    <AccessibleText>{text}</AccessibleText>
+                  </a>
+                ))}
+            </SocialWrapper>
+          </p>
+          <p className="footer-item-text">
+          ____________________________________________
+          </p>
+          <h3 className="footer-item-text">
             {authorName} © {new Date().getFullYear()}
           </h3>
         </div>
-        {footerLinks.map((column, i) => {
-          return <FooterColumn column={column} key={`footer-column-${i}`} />
-        })}
       </nav>
     </FooterWrapper>
   )
---
+++ src/components/Social.js
@@ -0,0 +1,24 @@
+import { Twitter, Linkedin, Instagram, GitHub } from 'react-feather';
+
+export const SOCIAL_LINKS = [
+  {
+    url: 'https://www.linkedin.com/in/anuj-kapoor-642387158/',
+    text: 'Connect on Linkedin',
+    Icon: Linkedin,
+  },
+  {
+    url: 'https://twitter.com/AnujKaps',
+    text: 'Follow me on Twitter',
+    Icon: Twitter,
+  },
+  {
+    url: 'https://www.instagram.com/anuj_kaps/',
+    text: 'Follow me on Instagram',
+    Icon: Instagram,
+  },
+  {
+    url: 'https://github.com/Bearbobs',
+    text: 'Follow me on GitHub',
+    Icon: GitHub,
+  },
+];
